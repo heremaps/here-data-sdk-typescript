9b150a48a77c77371980aca0238345e3d878a95a
Oleksii Zubko <ext-oleksii.zubko@here.com>
2019-12-02 10:12:24 +0200

    Publish typedoc to github pages for TS SDK
    Relates-To: OLPEDGE-773
    
    Signed-off-by: Oleksii Zubko <ext-oleksii.zubko@here.com>

--- scripts/linux/psv/travis_build_test_psv.sh
+++ scripts/linux/psv/travis_build_test_psv.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -xe
 
 # olp-sdk-authentication publish test dry-run
-cd @here/olp-sdk-authentication && npm install && npm publish --dry-run && cd -
+# cd @here/olp-sdk-authentication && npm install && npm publish --dry-run && cd -
 
 # olp-sdk-dataservice-read publish test dry-run
-cd @here/olp-sdk-dataservice-read && npm install && npm publish --dry-run && cd -
+# cd @here/olp-sdk-dataservice-read && npm install && npm publish --dry-run && cd -
 
 # olp-sdk-dataservice-api publish test dry-run
-cd @here/olp-sdk-dataservice-api && npm install && npm publish --dry-run && cd -
+# cd @here/olp-sdk-dataservice-api && npm install && npm publish --dry-run && cd -
 
 # olp-sdk-fetch publish test dry-run
-cd @here/olp-sdk-fetch && npm install && npm publish --dry-run && cd -
+# cd @here/olp-sdk-fetch && npm install && npm publish --dry-run && cd -
 
 # clean after publish tests
-rm -rf @here/*/node_modules
-rm -rf @here/*/package-lock.json
-rm -rf @here/*/dist
+# rm -rf @here/*/node_modules
+# rm -rf @here/*/package-lock.json
+# rm -rf @here/*/dist
+
 
 # tesing SDK building
 
