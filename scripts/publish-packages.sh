523853491978d62a9b6fb82213507435fce8f6a8
Bautista, Iryna <ext-iryna.bautista@here.com>
2019-12-17 15:44:04 +0200

    Add Travis job and script for NPM publish
    Job will trigger on tag creation.
    Script require NPM_TOKEN to be defined as variable.
    
    Relates-To: OLPEDGE-895
    
    Signed-off-by: Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>

---
+++ scripts/publish-packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+#Simple script that bundles the publishing of packages
+#to be run from Travis
+
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > ~/.npmrc
+#npx lerna publish -y from-git --pre-dist-tag alpha
+echo "Fake publish"
