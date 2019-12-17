e4560ad3d72b5ba837dfc3ddf0904bb5b07f5ea8
Oleksii Zubko <ext-oleksii.zubko@here.com>
2019-12-17 16:00:10 +0200

    Add the integration testing for the bundles.
    This is a working solution for testing generated and published bundles.
    The mocha tesh framework is used for writing tests as it uses for writing unit tests.
    Also adds the running tests for generated bundles to the PSV job.
    
    Resolves: OLPEDGE-468, OLPEDGE-481
    Relates-To: OLPEDGE-845
    
    Signed-off-by: Oleksii Zubko <ext-oleksii.zubko@here.com>

---
+++ tests/integration/bundles/umd/olp-sdk-published.test.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { jsLoaderFactory } from "./utils";
+import { OlpSdkDataserviceApiTestCases } from "./olp-sdk-dataservice-api-testCases";
+import { OlpSdkAuthenticationTestCases } from "./olp-sdk-authentication-testCases";
+import { OlpSdkDataserviceReadTestCases } from "./olp-sdk-dataservice-read-testCases";
+
+describe("Test published olp-edge-datastore-api", () => {
+  before(jsLoaderFactory({url: "https://unpkg.com/@here/olp-sdk-dataservice-api/bundle.umd.min.js"}));
+
+  OlpSdkDataserviceApiTestCases.forEach(testCase => {
+    it(testCase.it, testCase.callback);
+  });
+});
+
+describe("Test published olp-edge-datastore-read", () => {
+  before(jsLoaderFactory({url: "https://unpkg.com/@here/olp-sdk-dataservice-read/bundle.umd.min.js"}));
+
+  OlpSdkDataserviceReadTestCases.forEach(testCase => {
+    it(testCase.it, testCase.callback);
+  });
+});
+
+describe("Test published olp-sdk-authentication", () => {
+  before(jsLoaderFactory({url: "https://unpkg.com/@here/olp-sdk-authentication/bundle.umd.min.js"}));
+
+  OlpSdkAuthenticationTestCases.forEach(testCase => {
+    it(testCase.it, testCase.callback);
+  });
+});
