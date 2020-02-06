2903383885e8735740302db14f10ba7ca69c19f5
Oleksii Zubko <ext-oleksii.zubko@here.com>
2020-02-06 12:38:31 +0200

    Fix publishing npm job after travis stuck
    Relates: OLPEDGE-1545
    
    Signed-off-by: Oleksii Zubko <ext-oleksii.zubko@here.com>

--- scripts/publish-packages.sh
+++ scripts/publish-packages.sh
@@ -4,9 +4,6 @@
 
 echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > ~/.npmrc
 
-# olp-sdk-authentication publish
-cd @here/olp-sdk-authentication && npm install && npm publish && cd -
-
 # olp-sdk-fetch publish
 cd @here/olp-sdk-fetch && npm install && npm publish && cd -
 
@@ -15,5 +12,9 @@ cd @here/olp-sdk-dataservice-api && npm install && npm publish && cd -
 
 # olp-sdk-dataservice-read publish
 cd @here/olp-sdk-dataservice-read && npm install && npm publish && cd -
+
+# olp-sdk-authentication publish
+cd @here/olp-sdk-authentication && npm install && npm publish && cd -
+
 
 echo 'Publish done!'
