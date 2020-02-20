a1d3dc1e38cb334895f625645d6ffbab96773f7c
ashevchu <ext-andrii.shevchuk@here.com>
2020-02-20 13:23:05 +0200

    Add unsubscribe method to the StreamLayerClient
    This method deletes a subscription to a layer. This operation removes the subscription from the service.
    * Add UnsubscribeRequest
    * Add unit tests to the UnsubscribeRequest
    * Add unsubscribe method to the StreamLayerClient
    * Add unit tests to mentioned method
    
    Resolves: OLPEDGE-1526
    Signed-off-by: ashevchu <ext-andrii.shevchuk@here.com>

---
+++ @here/olp-sdk-dataservice-read/test/unit/UnsubscribeRequest.test.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import * as chai from "chai";
+import sinonChai = require("sinon-chai");
+
+import { UnsubscribeRequest } from "../../lib";
+
+chai.use(sinonChai);
+
+const assert = chai.assert;
+const expect = chai.expect;
+
+describe("SubscribeRequest", () => {
+    it("Should initialize", () => {
+        const request = new UnsubscribeRequest();
+
+        assert.isDefined(request);
+        expect(request).be.instanceOf(UnsubscribeRequest);
+    });
+
+    it("Should set parameters", () => {
+        const mockMode = "parallel";
+        const mockSubId = "1111111111";
+        const request = new UnsubscribeRequest();
+        const requestWithMode = request.withMode(mockMode);
+        const requestWithSub = request.withSubscriptionId(mockSubId);
+
+        expect(requestWithMode.getMode()).to.be.equal(mockMode);
+        expect(requestWithSub.getSubscriptionId()).to.be.equal(mockSubId);
+    });
+
+    it("Should get parameters with chain", () => {
+        const mockMode = "parallel";
+        const mockSubId = "1111111111";
+
+        const request = new UnsubscribeRequest()
+            .withMode(mockMode)
+            .withSubscriptionId(mockSubId);
+
+        expect(request.getMode()).to.be.equal(mockMode);
+        expect(request.getSubscriptionId()).to.be.equal(mockSubId);
+    });
+});
