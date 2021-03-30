b867a9668d389bd4b497a959fd9f5198714b6f94
Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>
2021-03-30 17:08:54 +0300

    Enable Gitlab SAST of SDK TS
    Enable new Gitlab Scanner for security scans.
    According to official Gitlab docs:
    https://docs.gitlab.com/13.9/ee/user/application_security/sast/
    
    Relates-To: OLPEDGE-2416
    
    Signed-off-by: Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>

--- scripts/publish-packages.sh
+++ scripts/publish-packages.sh
@@ -61,6 +61,6 @@ sleep 60
 echo 'Publish verification...'
 yarn
 yarn bootstrap
-npm run --silent build
-npm run --silent http-server-testing-bundles & npm run --silent test-published-bundles
+npm run build
+npm run http-server-testing-bundles & npm run test-published-bundles
 echo 'Publish verification done! '
